d6f98f146827997b45f5977fe70c1b6c6e421e1c
Jerin Philip <jphilip@ed.ac.uk>
2021-03-24 18:32:42 +0000

    Syncing docs with jp/word-alignment

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.0.1 2021-03-24',
+    VERSION: 'v0.0.0 2021-03-24',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
