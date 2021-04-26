f45f0b09b6da88b270aa6144d527aa2076631e2d
Jerin Philip <jphilip@ed.ac.uk>
2021-04-26 14:58:42 +0000

    Updates to docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.0.0 2021-04-03',
+    VERSION: 'v0.0.0 2021-04-26',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
