f724bf6981a82a2ab6a931034aaa9217f0f4c6cf
Jerin Philip <jphilip@ed.ac.uk>
2021-06-25 11:06:21 +0000

    maxLengthBreak_ -> wrapStep bugfix

--- src/translator/text_processor.cpp
+++ src/translator/text_processor.cpp
@@ -121,7 +121,7 @@ void TextProcessor::wrap(Segment &segment, std::vector<string_view> &wordRanges,
 
     // Restrict the range within bounds.
     size_t left = segment.size() - offset;
-    size_t diff = std::min(maxLengthBreak_, left);
+    size_t diff = std::min(wrapStep, left);
 
     segments.emplace_back(start, start + diff);
 
