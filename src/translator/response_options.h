e1e219ae1dcbcb0dc8e3a0a3980704ccc3c56a6c
Jerin Philip <jphilip@ed.ac.uk>
2022-07-07 18:26:27 +0100

    Remove unused (Response)Options
    ConcatStrategy was a feature flag @jerinphilip introduced to argue the
    case for faithful translation. Faithful translation is now default,
    except HTML tends to tamper with the notion slightly.
    
    SentenceMappings is really unused now, because we always store sentence
    information given we have `Annotation` by default.
    
    Remove usage of concatStrategy in library.

--- src/translator/response_options.h
+++ src/translator/response_options.h
@@ -5,28 +5,12 @@
 namespace marian {
 namespace bergamot {
 
-enum ConcatStrategy {
-  /// Target text is constructed faithful to the source-text  structure.
-  FAITHFUL,
-
-  /// Target text is concatenated by a space.
-  SPACE
-};
-
 /// ResponseOptions dictate how to construct a Response for an input string of
 /// text to be translated.
 struct ResponseOptions {
   bool qualityScores{false};  ///< Include quality-scores or not.
   bool alignment{false};      ///< Include alignments or not.
-
-  bool HTML{false};  /// Remove HTML tags from text and insert in output.
-
-  /// Whether to include sentenceMappings or not. Alignments require
-  /// sentenceMappings and are available irrespective of this option if
-  /// `alignment=true`.
-  bool sentenceMappings{false};
-
-  ConcatStrategy concatStrategy{ConcatStrategy::FAITHFUL};
+  bool HTML{false};           ///< Remove HTML tags from text and insert in output.
 };
 
 }  // namespace bergamot
